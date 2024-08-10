1412948b551b125eebf615b6bc9737488f7026ca
hsrokon <hsrokonofficial@gmail.com>
2024-08-10 12:02:03 +0600

    created Header.js file > ex/imported it>setted it up +and a simple heading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      My Project
+      {/* In order to bring the imported component into our application  we've import it like this <../> */}
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+  // returning some html/JSX 
+  return (
+    <div>
+      <h1>
+        My Header
+      </h1>
+    </div>
+  )
+}
+
+export default Header;
