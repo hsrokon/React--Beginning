a51a493627a8bdbb99e3665afcb47f7ef94774c1
hsrokon <hsrokonofficial@gmail.com>
2024-08-10 18:35:58 +0600

    Did some basic set-up, created 3 folders (assets,components,pages) Learned very basic stuff like shortcuts of extnsn  -added data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './components/Header.js';
+// imp default import shortcut
+import NotesListPage from './pages/NotesListPage.js';
 
 function App() {
   return (
     <div className="App">
       {/* In order to bring the imported component into our application  we've import it like this <../> */}
-      <Header/>
+      <Header />
+      <NotesListPage />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,13 @@ const Header = () => {
   // returning some html/JSX 
   return (
     <div>
+      {/* we must only have one parent div | we can multiple chil div */}
+      {/* we can just use a fragment <></> without having anything like div that will also work */}
+
       <h1>
         My Header
       </h1>
+      
     </div>
   )
 }
