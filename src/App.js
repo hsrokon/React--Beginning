2d42b8534a6bb7d837a14fc75f1814fb11871182
hsrokon <hsrokonofficial@gmail.com>
2024-08-10 22:54:29 +0600

    Fixed and learned the Router (Routes> Route) and
    import {
      BrowserRouter as Router,
      Route,
      Routes
    } from "react-router-dom";

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {
-  createBrowserRouter,
+  BrowserRouter as Router,
   Route,
-  Router
+  Routes
 } from "react-router-dom";
 
 import './App.css';
@@ -13,13 +13,18 @@ import NotePage from './pages/NotePage.js';
 function App() {
   return (
     <Router>
-        <div className="App">
+      <div className="App">
         {/* In order to bring the imported component into our application  we've import it like this <../> */}
         <Header />
-        <NotesListPage />
+        <Routes>
+          <Route path="/" element={<NotesListPage />} />
+          <Route path="/note" element={<NotePage />} />
+        </Routes>
       </div>
     </Router>
-    
+    //with this Route we specify the path and the component that should be rendered out
+    // "/" means homepahe ex. 'hsrokon.com/'
+    // "/note" will take me to note page
   );
 }
 
