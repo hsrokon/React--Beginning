1c40febdef80f8a5801ad87e413135674ba771f5
hsrokon <hsrokonofficial@gmail.com>
2024-08-11 09:44:15 +0600

    Fixed some router issues in NotePage.js because of v5 he used which updated to v6 now

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const NotePage = () => {
+  let id = useParams()
+  console.log("noteId:", id)
   return (
     <div>
       <h1>This is a single note page</h1>
