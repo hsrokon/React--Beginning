fb69e37bacdcb0e6d946dd0119c6868c60f813fe
hsrokon <hsrokonofficial@gmail.com>
2024-08-11 09:55:59 +0600

    Added link to each notes with unique id
    import { Link } from 'react-router-dom'
    
    <Link to={`/note/${note.id}`}>
          <h3>{note.body}</h3>
        </Link>

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // props is fixed data which we can't modify/updaye
 // props are properties which we can pass down to a child component
 const ListItem = ({note}) => {
-  // console.log('PROPS:', props);
+  // Link is like <a> tag in html  to is 'href'
+  //we're giving link to each note with id
   return (
-    <div>
+    <Link to={`/note/${note.id}`}>
       <h3>{note.body}</h3>
-    </div>
+    </Link>
   )
 }
 
