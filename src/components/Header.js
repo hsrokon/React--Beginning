44e50bc8687a77a38d2af0c65cf27be4e1279d6a
hsrokon <hsrokonofficial@gmail.com>
2024-08-11 11:10:23 +0600

    Decorated NotePage.js( for individual note) by connecting CSS with specific class and added textarea to view it as editable

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@
 const Header = () => {
   // returning some html/JSX 
   return (
-    <div>
+    <div className="app-header">
       {/* we must only have one parent div | we can multiple chil div */}
       {/* we can just use a fragment <></> without having anything like div that will also work */}
 
       <h1>
-        My Notes
+        Notes List
       </h1>
       
     </div>
--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import notes from '../assets/data'
+import { Link } from 'react-router-dom'
+import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg'
+//we can import images as components
 
 const NotePage = () => {
   let {id} = useParams()
@@ -10,8 +13,17 @@ const NotePage = () => {
   //imported all the notes> used id> ran a filter to find specific note> rendered that in the body
   
   return (
-    <div>
-      <p>{note?.body}</p>
+    <div className='note'>
+      <div className='note-header'>
+        <h3>
+            <Link to="/">
+              <ArrowLeft />
+            </Link>
+        </h3>
+      </div>
+      <textarea value={note?.body}>
+        
+      </textarea>
     </div>
     //that '?' is not to through an error if id doesn't exist
   )
